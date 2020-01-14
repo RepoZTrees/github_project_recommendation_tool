fef1d86769e41ed311953584e92a117b6be5fd6f
Vik <csimm3330.com>
2020-01-14 19:14:21 +0530

    added get_repos() and get_fav_language().

--- github_project.py
+++ github_project.py
@@ -2,22 +2,40 @@ import config
 import requests
 import json
 
-username = ''
-token = config.gh_api_key
+#repos_url = 'https://api.github.com/user/repos'
 
-repos_url = 'https://api.github.com/user/repos'
-
-gh_session = requests.Session()
-gh_session.auth = (username, token)
-
-repos = json.loads(gh_session.get(repos_url).text)
-
-for repo in repos:
-    json_format = json.dumps(repo,indent=4)
-    repo_name = repo['name']
-    repo_programming_language_name = repo['language']
-    print(repo_name,repo_programming_language_name)
+def get_repos(user):
     
+    repo_language = []
+    username = user
+    token = config.gh_api_key
+    repos_url = 'https://api.github.com/user/repos'
+    gh_session = requests.Session()
+    gh_session.auth = (username, token)
+    repos = json.loads(gh_session.get(repos_url).text)
+    for repo in repos:
+        json_format = json.dumps(repo,indent=4)
+        repo_language.append(repo['language'])
+    return repo_language
 
+#a = get_repos('repoZtrees')
+#print(a)
 
+#------
 
+def get_fav_language(user_name):
+
+    languages = get_repos(user_name)
+    #print ("Hi", languages)
+    counter = 0
+    num = languages[0]   
+   
+    for i in languages:
+        curr_frequency = languages.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+    return num
+        
+a = get_fav_language('RepoZTrees')
+print(a)
