7301f35172fbe5e752d5bb3e21466a9c9ddd0dfb
Vik <csimm3330.com>
2020-01-14 19:10:39 +0530

    added test_fav_language().Test passed.

--- test_github_project.py
+++ test_github_project.py
@@ -2,5 +2,6 @@ import github_project
 
 def test_fav_language():
 
-    assert github_project.get_fav_language(['python','python','python','ruby','java','c+','javascript']) == 'python'
+    #assert github_project.get_fav_language(['python','python','python','ruby','java','c','javascript']) == 'Python'
+    assert github_project.get_fav_language('repoZTrees') == 'Python'
     
