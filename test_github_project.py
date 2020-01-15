6cf27e6f0536a77ceafc599205634598d2c3e3b6
Vik <csimm3330.com>
2020-01-15 13:16:04 +0530

    added test_recommend_project().Test failed.

--- test_github_project.py
+++ test_github_project.py
@@ -2,6 +2,10 @@ import github_project
 
 def test_fav_language():
 
-    #assert github_project.get_fav_language(['python','python','python','ruby','java','c','javascript']) == 'Python'
     assert github_project.get_fav_language('repoZTrees') == 'Python'
+    
+def test_recommend_project():
+    actual_value = ['Python','Ruby','Java','Python','Javscript']
+    expected_value = 'Python'
+    assert actual_value == expected_value
     
