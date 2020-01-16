2317670350c396576d840ac17fac1dd1a5a584dd
Vik <csimm3330.com>
2020-01-16 17:10:13 +0530

    added recommend_project().

--- github_project.py
+++ github_project.py
@@ -29,13 +29,45 @@ def get_fav_language(user_name):
     #print ("Hi", languages)
     counter = 0
     num = languages[0]   
-   
+
     for i in languages:
-        curr_frequency = languages.count(i)
-        if(curr_frequency> counter):
-            counter = curr_frequency
-            num = i
-    return num
+        prog_languages = languages.count(i)
+        if(prog_languages>counter):
+            counter = prog_languages
+            pl = i
+    #print('winner is:',pl)
+    return pl 
         
-a = get_fav_language('RepoZTrees')
-print(a)
+#a = get_fav_language('RepoZTrees')
+#print(a)
+
+def recommend_project(user, order_by='update'):
+    project_name = []
+    search_language = get_fav_language(user)
+    sort_by = 'updated'
+    token = config.gh_api_key 
+
+    #r = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort=stars&order=desc'
+    #r = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort=updated&order=desc'
+    r = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort='+sort_by+'&order=desc'
+    gh_session = requests.Session()
+    gh_session.auth = (user, token)
+    repos = json.loads(gh_session.get(r).text)
+    
+    for project in repos['items']:
+        json_format = json.dumps(project,indent=4)
+        project_name.append(project['full_name'])
+    return project_name
+    #print(type(project_name))
+
+rec = recommend_project('RepoZTrees')
+print(rec)
+
+#------
+
+#def sort_query():
+   # by update, by stars, help wanted
+    
+    
+
+#-------
