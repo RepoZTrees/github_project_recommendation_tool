a4952033079ce613be454377f016dadde70d1821
Vik <csimm3330.com>
2020-01-16 19:02:15 +0530

    added connect_github().Made a few changes. Created a connect_github function for requests.Session() instead of calling it in each function.

--- github_project.py
+++ github_project.py
@@ -2,17 +2,23 @@ import config
 import requests
 import json
 
-#repos_url = 'https://api.github.com/user/repos'
+#-------------
+
+def connect_github(user):
+   
+    token = config.gh_api_key
+    gh_session = requests.Session()
+    gh_session.auth = (user, token)
+    return gh_session
+
+#---------------
 
 def get_repos(user):
-    
-    repo_language = []
-    username = user
-    token = config.gh_api_key
+   
     repos_url = 'https://api.github.com/user/repos'
-    gh_session = requests.Session()
-    gh_session.auth = (username, token)
+    gh_session = connect_github(user)
     repos = json.loads(gh_session.get(repos_url).text)
+    repo_language = []    
     for repo in repos:
         json_format = json.dumps(repo,indent=4)
         repo_language.append(repo['language'])
@@ -21,12 +27,11 @@ def get_repos(user):
 #a = get_repos('repoZtrees')
 #print(a)
 
-#------
+#-----------
 
 def get_fav_language(user_name):
 
     languages = get_repos(user_name)
-    #print ("Hi", languages)
     counter = 0
     num = languages[0]   
 
@@ -35,39 +40,25 @@ def get_fav_language(user_name):
         if(prog_languages>counter):
             counter = prog_languages
             pl = i
-    #print('winner is:',pl)
     return pl 
         
 #a = get_fav_language('RepoZTrees')
 #print(a)
 
-def recommend_project(user, order_by='update'):
-    project_name = []
+#---------
+
+def recommend_project(user):
+    project_name = [] 
     search_language = get_fav_language(user)
     sort_by = 'updated'
-    token = config.gh_api_key 
-
-    #r = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort=stars&order=desc'
-    #r = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort=updated&order=desc'
-    r = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort='+sort_by+'&order=desc'
-    gh_session = requests.Session()
-    gh_session.auth = (user, token)
-    repos = json.loads(gh_session.get(r).text)
-    
+    repos_url = 'https://api.github.com/search/repositories?q=language:'+search_language+'&sort='+sort_by+'&order=desc'
+    gh_session = connect_github(user)
+    repos = json.loads(gh_session.get(repos_url).text)
+     
     for project in repos['items']:
         json_format = json.dumps(project,indent=4)
         project_name.append(project['full_name'])
     return project_name
-    #print(type(project_name))
 
 rec = recommend_project('RepoZTrees')
 print(rec)
-
-#------
-
-#def sort_query():
-   # by update, by stars, help wanted
-    
-    
-
-#-------
