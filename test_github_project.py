dcf9dfa3faede53e3fbe2b3197fa32046ccd8cdb
Vik <csimm3330.com>
2020-01-22 17:34:28 +0530

    deleted test_recommend_project_by_language_and_sort_by_update() and test_recommend_project_by_commit_date().

--- test_github_project.py
+++ test_github_project.py
@@ -3,9 +3,3 @@ import github_project
 def test_fav_language():
 
     assert github_project.get_fav_language('repoZTrees') == 'Python'
-    
-def test_recommend_project():
-    actual_value = ['Python','Ruby','Java','Python','Javscript']
-    expected_value = 'Python'
-    assert actual_value == expected_value
-    
