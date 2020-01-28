d31859c46ae51443dcb963ffafec585f3dc6209d
Vik Shah <csimm3330@gmail.com>
2020-01-28 17:43:16 +0530

    Added test_query_params().

--- test_github_project.py
+++ test_github_project.py
@@ -4,3 +4,10 @@ def test_fav_language():
 
     assert github_project.get_fav_language('repoZTrees') == 'Python'
     
+
+def test_query_params():
+    
+    assert github_project.query_params() == {'q':'language:python', 'sort':'updated','order':'desc'}  
+    assert github_project.query_params('ruby') == {'q':'language:ruby', 'sort':'updated','order':'desc'}
+
+    
